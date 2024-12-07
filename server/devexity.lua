327a887d4ed10454240188f0db45f0ccfe7bacd6
Devexity <91433688+Devexity-fivem@users.noreply.github.com>
2024-12-07 06:42:42 -0600

    Update devexity.lua

--- server/devexity.lua
+++ server/devexity.lua
@@ -1,5 +1,12 @@
-print("DDDDD   EEEEE  V   V  EEEEE  X   X  III  TTTTT  Y   Y")
-print("D    D  E      V   V  E       X X    I     T     Y Y ")
-print("D    D  EEEE   V   V  EEEE     X     I     T      Y  ")
-print("D    D  E      V   V  E       X X    I     T      Y  ")
-print("DDDDD   EEEEE   V V   EEEEE  X   X  III    T      Y  ")
+AddEventHandler("onResourceStart", function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        Citizen.CreateThread(function()
+            Citizen.Wait(15000) -- Wait for 15000 milliseconds (15 seconds) or adjust as needed
+            print("DDDDD   EEEEE  V   V  EEEEE  X   X  III  TTTTT  Y   Y")
+            print("D    D  E      V   V  E       X X    I     T     Y Y ")
+            print("D    D  EEEE   V   V  EEEE     X     I     T      Y  ")
+            print("D    D  E      V   V  E       X X    I     T      Y  ")
+            print("DDDDD   EEEEE   V V   EEEEE  X   X  III    T      Y  ")
+        end)
+    end
+end)
